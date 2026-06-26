e8c13969ef8951ec7373869f3186c279c3b81bef
Kaneko keeQ <kaneko@keeq.co.jp>
2026-06-26 14:31:50 +0900

    修正

--- build.bat
+++ build.bat
@@ -1,13 +1,12 @@
 @echo off
-chcp 65001 >nul
 cd /d "%~dp0"
 echo ================================================
-echo  Tailwind CSS をビルドしています...
+echo  Building Tailwind CSS...
 echo ================================================
 tools\tailwindcss.exe -i src\input.css -o css\tailwind.css --minify
 echo.
 echo ================================================
-echo  完了しました！ css\tailwind.css を更新しました。
-echo  ※HTMLを編集したら、このファイルを実行してください。
+echo  Done! css\tailwind.css has been updated.
+echo  Run this file again after editing any HTML file.
 echo ================================================
 pause
